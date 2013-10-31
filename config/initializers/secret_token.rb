a4051678718778e9954d35052b784c98fca18fda
Pedro Magni <pmagnis@gmail.com>
2013-10-30 21:43:12 -0300

    Adding ActiveAdmin with Pin resource

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CursoMktg::Application.config.secret_token = '572a88ac140f662591d3d9e35f347be72326e115e73bfe967ec5a498510b8f54e81ac37ee9228f0afc757b802fb4c266673602464dba9f23ad314cdf289e2264'
